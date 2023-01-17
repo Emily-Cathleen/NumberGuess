ff028b4892f62c5ccead21debccab8391abebe6d
Emily Corcoran <cathleencorcoran@gmail.com>
2023-01-17 14:32:33 -0500

    initial commit

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,53 @@
+import { TextInput, View, StyleSheet } from "react-native";
+
+import PrimaryButton from "../components/PrimaryButton";
+
+function StartGameScreen() {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <View style={styles.buttonContainer}>
+        <PrimaryButton>Reset</PrimaryButton>
+        <PrimaryButton>Confirm</PrimaryButton>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 8,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 5 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+    marginHorizontal: 24,
+    marginTop: 100,
+    padding: 16,
+    backgroundColor: "#4e0329",
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeigt: "bold",
+    textAlign: "center",
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+  },
+});
+
+export default StartGameScreen;
