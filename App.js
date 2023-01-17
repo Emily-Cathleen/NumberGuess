9901a4545c6647d60d87d16974441652dee43fb4
Emily Corcoran <cathleencorcoran@gmail.com>
2023-01-17 16:19:12 -0500

    Linear Gradient not working

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
-import StartGameScreen from './screens/StartGameScreen';
+import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
   return (
-   <StartGameScreen />
-   )
+    <LinearGradient colors={[ '#34e0329', '#ddb52f' ]} style={styles.screen}>
+      <StartGameScreen />
+      </LinearGradient>
+    
+  );
 }
- 
+
 const styles = StyleSheet.create({
-  
+  screen: {
+    flex: 1
+  },
 });
